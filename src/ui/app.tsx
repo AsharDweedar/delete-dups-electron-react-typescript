512c31c5749df617086793be593c0045d7a7ae48
AsharDweedar <Ashar.Dweedar@gmail.com>
2018-05-18 15:42:57 +0300

    added input field from belle library

--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import NavBar from './components/nav_bar';
+import {TextInput} from 'belle'
 
 export class App extends React.Component<undefined, undefined> {
   render() {
     return (
       <div>
         <h2>Welcome to React with Typescript!</h2>
+        <TextInput defaultValue="Update here and see how the input grows …" />
         <NavBar />
       </div>
     );
