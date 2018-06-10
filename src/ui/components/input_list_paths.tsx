b6f0b220bcbede3e86e27ca7f5fbd9424bf1649a
AsharDweedar <Ashar.Dweedar@gmail.com>
2018-06-10 14:55:21 +0300

    change ow to get path

--- src/ui/components/input_list_paths.tsx
+++ src/ui/components/input_list_paths.tsx
@@ -9,12 +9,11 @@ export default class InputListPaths extends React.Component<any, {}> {
     constructor(props: PropsInterface) {
         super(props);
         this.state = {
-            [props.name]: [{ value: "Ashar" }, { value: "Aseel" }]
+            "paths": [{ value: "Ashar" }, { value: "Aseel" }]
         }
     }
     onBrows(e : {target: {}}) {
-    // onBrows(e: HTMLInputElement) {
-        console.log(e.target["value"]) // C:\fakepath\options.tsx
+        console.log(e.target["files"][0]["path"]) // C:\realpath\options.tsx
     }
     onInsert({ value }: { value: string}) {
         this.state[this.props.name].push(value)
@@ -23,7 +22,7 @@ export default class InputListPaths extends React.Component<any, {}> {
         let p = this.props
         return (
             <div style={{ width: "40%", display: "inline-block" }}>
-                <input type="file" onChange={this.onBrows} webkitdirectory />
+                <input id="newFolder" type="file" onChange={this.onBrows} webkitdirectory />
                 <TextInput defaultValue="Type folders paths to include in the searchs" />
                 <List list={this.state["paths"]} name={"paths"} />
             </div>
