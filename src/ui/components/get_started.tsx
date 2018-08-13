fd40aec56b0e4d9683be373c284c2644ee1be03a
AsharDweedar <Ashar.Dweedar@gmail.com>
2018-08-13 10:15:01 +0300

    add scan button

--- src/ui/components/get_started.tsx
+++ src/ui/components/get_started.tsx
@@ -11,19 +11,25 @@ export default class GetStarted extends React.Component<any, {}> {
   render() {
     return (
       <div>
-        <Options />
         <div style={{
-          "justifyContent": "center", display: "flex", alignContent: "flex-start"}}>
-            <InputListPaths
-              add={this.props.addPath}
-              paths={this.props["paths"]}
-            />
-            <InputListExtensions
-              add={this.props.addEXT}
-              ext={this.props["ext"]}
-              ExtCase={this.props.ExtCase}
-              ExtCaseChange={this.props.ExtCaseChange}
-            />
+          justifyContent: "center", display: "flex", alignContent: "center", padding: "0.5em"
+        }}>
+          <Options />
+          <a className="waves-effect waves-light btn-large right" style={{ margin: "2.5em" }}><i className="material-icons left">cloud</i>Start Scanning</a>
+        </div>
+        <div style={{
+          "justifyContent": "center", display: "flex", alignContent: "flex-start"
+        }}>
+          <InputListPaths
+            add={this.props.addPath}
+            paths={this.props["paths"]}
+          />
+          <InputListExtensions
+            add={this.props.addEXT}
+            ext={this.props["ext"]}
+            ExtCase={this.props.ExtCase}
+            ExtCaseChange={this.props.ExtCaseChange}
+          />
         </div>
       </div>
     );
