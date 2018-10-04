b6bbb6e5487fe17e22b4464f50ef6e2238fa7fbd
AsharDweedar <Ashar.Dweedar@gmail.com>
2018-10-04 16:48:53 +0300

    editing to my code

--- src/src/main.tsx
+++ src/src/main.tsx
@@ -5,16 +5,44 @@ import { ConnectedRouter } from 'react-router-redux';
 import { createBrowserHistory } from 'history';
 import { configureStore } from 'app/store';
 import { App } from './app';
+import { AppContainer } from 'react-hot-loader';
 
 // prepare store
 const history = createBrowserHistory();
 const store = configureStore(history);
 
 ReactDOM.render(
+  <AppContainer>
   <Provider store={store}>
     <ConnectedRouter history={history}>
       <App />
     </ConnectedRouter>
-  </Provider>,
+  </Provider>
+  </AppContainer>,
   document.getElementById('root')
 );
+
+
+const { remote } = require('electron')
+const { Menu, MenuItem } = remote
+const menu = new Menu();
+
+menu.append(new MenuItem({ role: 'about' }));
+menu.append(new MenuItem({ type: 'separator' }));
+menu.append(new MenuItem({ role: 'services', submenu: [{ label: 'customized event', click() { console.log('customized event clicked, this goes to the terminal') } }] }));
+menu.append(new MenuItem({ type: 'separator' }));
+menu.append(new MenuItem({ role: 'hide' }));
+menu.append(new MenuItem({ role: 'hideothers' }));
+menu.append(new MenuItem({ role: 'unhide' }));
+menu.append(new MenuItem({ type: 'separator' }));
+menu.append(new MenuItem({ role: 'quit' }));
+menu.append(new MenuItem({ label: 'MenuItem2', type: 'checkbox', checked: true }));
+
+window.addEventListener('contextmenu', (e: any) => {
+  e.preventDefault()
+  menu.popup({ window: remote.getCurrentWindow() })
+}, false)
+
+
+
+if (module.hot) { module.hot.accept(render); }
