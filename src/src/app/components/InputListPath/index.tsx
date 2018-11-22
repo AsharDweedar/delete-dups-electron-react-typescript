243f0d182c72f689e25ca448571abd259138a2ce
AsharDweedar <Ashar.Dweedar@gmail.com>
2018-11-22 15:29:45 +0200

    select folder

--- src/src/app/components/InputListPath/index.tsx
+++ src/src/app/components/InputListPath/index.tsx
@@ -16,51 +16,70 @@ export namespace InputListPath {
 }
 
 export class InputListPath extends React.Component<InputListPath.Props> {
-         static defaultProps: Partial<InputListPath.Props> = { paths: [] };
+  static defaultProps: Partial<InputListPath.Props> = { paths: [] };
 
-         // export class InputListPath extends React.Component<InputListPath.Props> {
-         constructor(props: InputListPath.Props, context?: any) {
-           super(props, context);
-           console.log(props);
-         }
-         //   componentDidMount() {
-         // let ele = document.getElementById('selector');
-         // if (!!ele) {
-         //   ele.setAttribute('webkitdirectory', 'true');
-         // }
-         // this.refs.x.directory = true;
-         // this.refs.x.webkitdirectory = true;
-         //   }
-         show(e: any) {
-           // const { dialog } = require('electron').remote;
-           // var [path] = dialog.showOpenDialog({
-           //   properties: ['openDirectory'],
-           // });
-           // this.props.actions.addPath({ path });
-           console.log('called ', e);
-           // var remote = require('remote');
-           // var dialog = remote.require('electron').dialog;
-
-           // var path = dialog.showOpenDialog({
-           //   properties: ['openDirectory']
-           // });
-
-           // console.log(path);
-         }
-         render() {
-           const { paths } = this.props;
-           return <div style={{ display: 'inline-block', alignItems: 'stretch', margin: '50px', maxWidth: '45%' }}>
-               <ul className="collection with-header">
-                 <li className="collection-header">
-                   <h4 style={{ display: 'inline-block' }}>Paths</h4>
-                   <input type="file" onClick={this.show.bind(this)} />
-                   <button onClick={this.show.bind(this)} style={{ float: 'right' }} className="btn-floating btn-large waves-effect waves-light green" />
-                 </li>
-                 <ListPath paths={paths} />
-               </ul>
-             </div>;
-         }
-       }
+  // export class InputListPath extends React.Component<InputListPath.Props> {
+  constructor(props: InputListPath.Props, context?: any) {
+    super(props, context);
+    console.log("input list path component props", props);
+  }
+  componentDidMount() {
+    let ele = document.getElementById('selector');
+    if (!!ele) {
+      ele.setAttribute('webkitdirectory', 'true');
+      ele.setAttribute('mozdirectory', 'true');
+      ele.setAttribute('msdirectory', 'true');
+      ele.setAttribute('odirectory', 'true');
+      ele.setAttribute('directory', 'true');
+      ele.setAttribute('multiple', 'true');
+    }
+  }
+  show(e: any) {
+    console.log('called show func');
+    var ele = document.getElementById('selector') || {
+      click: () => {},
+      refs: { x: {} },
+    };
+    console.log(ele);
+    ele.click();
+  }
+  update_list(e: any) {
+    let file = e.target.files[0].path;
+    console.log('called onChange ', file);
+    this.props.actions.addPath(file);
+  }
+  render() {
+    const { paths } = this.props;
+    return (
+      <div
+        style={{
+          display: 'inline-block',
+          alignItems: 'stretch',
+          margin: '50px',
+          maxWidth: '45%',
+        }}
+      >
+        <ul className="collection with-header">
+          <li className="collection-header">
+            <h4 style={{ display: 'inline-block' }}>Paths</h4>
+            <input
+              type="file"
+              style={{ display: 'none' }}
+              id="selector"
+              onChange={this.update_list.bind(this)}
+            />
+            <button
+              onClick={this.show.bind(this)}
+              style={{ float: 'right' }}
+              className="btn-floating btn-large waves-effect waves-light green"
+            />
+          </li>
+          <ListPath paths={paths} />
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default connect<
   Pick<InputListPath.Props, 'paths'>,
